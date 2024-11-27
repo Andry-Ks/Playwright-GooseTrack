c0b5fa4acc142fcdedb6a6f58585fe5793ea3620
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-11-28 00:54:00 +0200

    added frontend test

---
+++ tests/frontend/loginPage.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import { validEmail, validPassword } from '../../playwright.config';
+
+test.describe('Positive login test', () => {
+  test('Login with valid data', async ({ page, baseURL }) => {
+
+    if (!validEmail || !validPassword) {
+        throw new Error('Email or password is not defined in the configuration.');
+      }
+
+    await page.goto(baseURL!);
+    await page.click('a[href="/login"]');
+    await expect(page.locator('button[type="submit"]')).toBeVisible();
+    await page.fill('input[name="email"]', validEmail);
+    await page.fill('input[name="password"]', validPassword);
+    await page.click('button[type="submit"]');
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.click('text=Log out');
+    
+  });
+});
