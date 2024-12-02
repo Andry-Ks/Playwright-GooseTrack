7e390beaa5a21c2b00c3cb495aeaf990b03f7580
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-12-02 16:08:31 +0200

    added backend logout tests

---
+++ tests/backend/logout.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+import { validEmail, validPassword } from '../../playwright.config';
+
+test.describe('User logout test', () => {
+  let accessToken: string;
+  test.beforeAll(async ({ request }) => {
+    const backendURL = test.info().config.metadata.backendURL;
+    const loginResponse = await request.post(`${backendURL}/user/login`, {
+      data: {
+        email: validEmail,
+        password: validPassword,
+      },
+    });
+
+    expect(loginResponse.status()).toBe(200);
+
+    const body = await loginResponse.json();
+    accessToken = body.data.accessToken;
+  });
+
+  test('Positive test: Logout user', async ({ request }) => {
+    const backendURL = test.info().config.metadata.backendURL;
+    const logoutResponse = await request.get(`${backendURL}/user/logout`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    expect(logoutResponse.status()).toBe(200);
+  });
+
+  test('Negative test: Logout user again with the same token', async ({ request }) => {
+    const backendURL = test.info().config.metadata.backendURL;
+    const logoutResponse = await request.get(`${backendURL}/user/logout`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    expect(logoutResponse.status()).toBe(401); // Unauthorized
+  });
+});
