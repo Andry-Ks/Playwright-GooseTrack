f81fd6ba4ddafd72adc6061df943ed5e6853e704
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-12-02 15:46:33 +0200

    added backend tests

--- tests/backend/login.spec.ts
+++ tests/backend/login.spec.ts
@@ -46,4 +46,28 @@ test.describe('Negative login tests', () => {
     expect(response.status()).toBe(401);
   });
 
+  test('Login with missing password', async ({ request }) => {
+    const backendURL = test.info().config.metadata.backendURL;
+    const response = await request.post(`${backendURL}/user/login`, {
+      data: {
+        email: validEmail,
+        password: "",
+      },
+    });
+
+    expect(response.status()).toBe(409);
+  });
+
+  test('Login with incorrect method', async ({ request }) => {
+    const backendURL = test.info().config.metadata.backendURL;
+    const response = await request.patch(`${backendURL}/user/login`, {
+      data: {
+        email: validEmail,
+        password: validPassword,
+      },
+    });
+
+    expect(response.status()).toBe(405); //The error is not being handled by the server. Developers need to be informed.
+  });
+
 });
